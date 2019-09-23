68191be75f1cd536c630af69dcb399cfe0d7aad8
mchalama <55072765+mchalama@users.noreply.github.com>
2019-09-23 10:47:43 -0400

    Assignment 3

---
+++ mchalama_3.R
@@ -0,0 +1,71 @@
+install.packages("lpSolveAPI")
+library(lpSolveAPI)
+help(add.constraint)
+
+### Weigelt Production Problem
+mylp <- make.lp(11, 9)
+
+# Set Objective Function
+set.objfn(mylp, c(420, 360, 300, 420, 360, 300, 420, 360, 300))
+
+# Set values for the rows
+set.row(mylp, 1, c(1, 1, 1), indices = c(1, 2, 3))
+set.row(mylp, 2, c(1, 1, 1), indices = c(4, 5, 6))
+set.row(mylp, 3, c(1, 1, 1), indices = c(7, 8, 9))
+set.row(mylp, 4, c(20, 15, 12), indices = c(1, 2, 3))
+set.row(mylp, 5, c(20, 15, 12), indices = c(4, 5, 6))
+set.row(mylp, 6, c(20, 15, 12), indices = c(7, 8, 9))
+set.row(mylp, 7, c(1, 1, 1), indices = c(1, 4, 7))
+set.row(mylp, 8, c(1, 1, 1), indices = c(2, 5, 8))
+set.row(mylp, 9, c(1, 1, 1), indices = c(3, 6, 9))
+set.row(mylp, 10, c(900, 900, 900, -750, -750, -750), indices = c(1, 2, 3, 4, 5, 6))
+set.row(mylp, 11, c(450, 450, 450, -750, -750, -750), indices = c(1, 2, 3, 7, 8, 9))
+
+# Set Right Hand side
+rhs <- c(750, 900, 450, 13000, 12000, 5000, 900, 1200, 750, 0 , 0)
+set.rhs(mylp, rhs)
+
+#set Constraint type
+set.constr.type(mylp, rep("<=", 9), constraints = 1:9)
+set.constr.type(mylp, rep("=", 2), constraints = 10:11)
+
+set.bounds(mylp, lower = rep(0, 9))
+
+
+# finally, name the decision variables (column) and constraints (rows)
+lp.colnames <- c("P1L", "P1M", "P1S", "P2L", "P2M", "P2S", "P3L", "P3M", "P3S")
+lp.rownames <- c("P1 Capacity", "P2 Capacity", "P3 Capacity", "P1 Storage", "P2 Storage", "P3 Storage", "Large Sales", "medium Sales", "Small Sales", "Excess_Capacity1", "Excess_Capacity2")
+dimnames(mylp) <- list(lp.rownames, lp.colnames)
+
+lp.control(mylp, sense="max")
+
+# view the linear program object to make sure it's correct
+mylp
+
+# now solve the model
+solve(mylp)
+# --> A return value of 0 indicates that the model was successfully solved.
+
+#objective function value
+get.objective(mylp)
+
+#Optimal decision Variable values
+get.variables(mylp)
+
+#Constraints
+get.constraints(mylp)
+
+# look at surplus for each constraint
+get.constraints(mylp) - rhs
+
+#Identify sensitivity
+get.sensitivity.rhs(mylp)
+
+get.dual.solution(mylp)
+
+get.sensitivity.obj(mylp)
+
+#Range of Shadow Prices and reduced cost
+cbind(get.sensitivity.rhs(mylp)$duals[1:11], get.sensitivity.rhs(mylp)$dualsfrom[1:11], get.sensitivity.rhs(mylp)$dualstill[1:11])
+
+cbind(get.sensitivity.rhs(mylp)$duals[12:20], get.sensitivity.rhs(mylp)$dualsfrom[12:20], get.sensitivity.rhs(mylp)$dualstill[12:20])
